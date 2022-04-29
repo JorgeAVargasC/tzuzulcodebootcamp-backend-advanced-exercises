7654dfcfbcae8b0cce1f0e4fe703c496149d7c5d
JorgeVargas <javargas216@unicauca.edu.co>
2022-04-29 10:25:22 -0500

    Update

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => {
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Hello World</h1>");
+});
+
+app.get("/contacto", (req, res) => {
 
     return res.json({
         nombre: "Jorge",
         correo: "javargas1209@gmail.com",
     })
-
 });
 
 app.listen(4000, () => {

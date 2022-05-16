2c7bfd3cea809462486f22a576ad7f5682648fd9
JorgeVargas <javargas216@unicauca.edu.co>
2022-05-16 10:44:42 -0500

    Google Cloud Run and MongoDB Atlas config

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose")
 
 async function connection(){
-    const conn = await mongoose.connect("mongodb://mongo/myDB")
+    // const conn = await mongoose.connect("mongodb://mongo/myDB")
+    const conn = await mongoose.connect("mongodb+srv://javargas:12345@cluster0.eapun.mongodb.net/myFirstDataBase?retryWrites=true&w=majority")
     console.log(conn)
     console.log("Connection established...")
 }
